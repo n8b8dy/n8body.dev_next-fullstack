db36013d81254e0a9fcdf386423830b024076750
n8b8dy <ruslanchernomorski@gmail.com>
2024-05-18 23:09:48 +0300

    Feat: Add Link to Navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,13 +1,19 @@
 import type { FC } from 'react'
-import { cn } from '@/utils/styles'
+
+import Link from 'next/link'
+
 import { Heading } from '@/components/typography/Heading'
 import { ThemeSwitch } from '@/components/buttons/ThemeSwitch'
+
+import { cn } from '@/utils/styles'
 
 export const Navbar: FC = () => {
   return (
     <div className={cn('w-full flex justify-center bg-neutral-50 dark:bg-neutral-950 bg-opacity-25 border-b-[1px] dark:border-neutral-800')}>
       <nav className={cn('w-full max-w-screen-2xl p-3 flex justify-between items-center')}>
-        <Heading tag="h4" className={cn('font-semibold')}>n8body.dev</Heading>
+        <Link href="/">
+          <Heading tag="h4" className={cn('font-semibold')}>n8body.dev</Heading>
+        </Link>
         <div>
           <ThemeSwitch/>
         </div>
