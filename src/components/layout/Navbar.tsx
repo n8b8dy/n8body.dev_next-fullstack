255bdfb5275181f4e7f726fa77d36faf16c51bc9
n8b8dy <ruslanchernomorski@gmail.com>
2024-05-18 23:50:49 +0300

    Style: Add custom :focus-visible outline

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -9,8 +9,8 @@ import { cn } from '@/utils/styles'
 export const Navbar: FC = () => {
   return (
     <div className={cn('w-full flex justify-center bg-neutral-50 dark:bg-neutral-950 bg-opacity-25 border-b-[1px] dark:border-neutral-800')}>
-      <div className={cn('w-full max-w-screen-2xl px-5 py-3 flex justify-between items-center')}>
-        <Link href="/" className={cn('text-3xl leading-tight font-semibold')}>n8body.dev</Link>
+      <div className={cn('w-full max-w-screen-2xl px-5 py-2 flex justify-between items-center')}>
+        <Link href="/" className={cn('p-1 text-3xl leading-tight font-semibold rounded')}>n8body.dev</Link>
 
         <nav>
         </nav>
