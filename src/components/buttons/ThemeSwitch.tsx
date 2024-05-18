c9d8188b0ac80b178ecd5cf241744e2bca7ac21d
n8b8dy <ruslanchernomorski@gmail.com>
2024-05-18 22:56:11 +0300

    Feat: Create ThemeSwitch for Navbar

---
+++ src/components/buttons/ThemeSwitch.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { FC, useState } from 'react'
+import { useEffect } from 'react'
+
+import { useTheme } from 'next-themes'
+import { BsMoonStars } from 'react-icons/bs'
+import { IoPartlySunnyOutline } from 'react-icons/io5'
+
+import { cn } from '@/utils/styles'
+
+export const ThemeSwitch: FC = () => {
+  const [mounted, setMounted] = useState(false)
+  const { resolvedTheme, setTheme } = useTheme()
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return <div className={cn('w-[40px] h-[40px] rounded animate-pulse bg-neutral-200 dark:bg-neutral-900')}></div>
+
+  return (
+    <button onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')} className={cn(
+      'p-2 rounded text-2xl transition',
+      'hover:bg-neutral-200 hover:dark:bg-neutral-900',
+      'active:bg-neutral-200 active:dark:bg-neutral-900 ',
+    )}>
+      {resolvedTheme === 'dark' ? <BsMoonStars/> : <IoPartlySunnyOutline/>}
+    </button>
+  )
+}
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,16 +1,15 @@
 import type { FC } from 'react'
 import { cn } from '@/utils/styles'
 import { Heading } from '@/components/typography/Heading'
+import { ThemeSwitch } from '@/components/buttons/ThemeSwitch'
 
 export const Navbar: FC = () => {
   return (
-    <div className={cn('w-full flex justify-center bg-neutral-50 dark:bg-neutral-950 bg-opacity-50 border-b-[1px] dark:border-neutral-800')}>
-      <nav className={cn('w-full max-w-screen-2xl p-3 flex justify-between')}>
+    <div className={cn('w-full flex justify-center bg-neutral-50 dark:bg-neutral-950 bg-opacity-25 border-b-[1px] dark:border-neutral-800')}>
+      <nav className={cn('w-full max-w-screen-2xl p-3 flex justify-between items-center')}>
         <Heading tag="h4" className={cn('font-semibold')}>n8body.dev</Heading>
         <div>
-          <button className="">
-
-          </button>
+          <ThemeSwitch/>
         </div>
       </nav>
     </div>
