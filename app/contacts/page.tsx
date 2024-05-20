35840a398fc44559feffc0e03ea29ce815d61377
n8b8dy <ruslanchernomorski@gmail.com>
2024-05-20 21:27:22 +0300

    Style: Improve Contacts page typography

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -10,8 +10,8 @@ import { cn } from '@/utils/styles'
 
 const Paragraphs = [
   { text: 'Burgas, Bulgaria', icon: <HiOutlineLocationMarker/> },
-  { text: 'contact@n8body.dev', icon: <TbMailCode/> },
-  { text: 'ruslanshamsutdinov.dev@gmail.com', icon: <TbMail/> },
+  { text: 'contact@n8body.dev', icon: <TbMailCode className={cn('relative top-[1px]')}/> },
+  { text: 'ruslanshamsutdinov.dev@gmail.com', icon: <TbMail className={cn('relative top-[1px]')}/> },
 ] as const
 
 const Links = [
@@ -29,9 +29,7 @@ export default function Contacts() {
           <h2 className={cn(
             'mb-4 text-3xl text-transparent font-semibold bg-clip-text',
             'animate-background-shine bg-gradient-FVW',
-          )}>
-            Write Me
-          </h2>
+          )}>Write Me</h2>
           <Form/>
         </div>
       </div>
@@ -41,31 +39,27 @@ export default function Contacts() {
           <h2 className={cn(
             'mb-4 text-3xl text-transparent font-semibold bg-clip-text',
             'animate-background-shine bg-gradient-FVW',
-          )}>
-            Contacts
-          </h2>
+          )}>Contacts</h2>
 
           <div className={cn('pb-4 flex flex-col gap-4 overflow-x-auto')}>
-            {Paragraphs.map(({ text, icon }) => <div key={text} className={cn('flex items-center gap-1')}>
-              <span className={cn('text-3xl mr-1')}>{icon}</span>
-              <span className={cn('text-xl')}>{text}</span>
+            {Paragraphs.map(({ text, icon }) => <div key={text} className={cn('flex items-center gap-1.5')}>
+              <span className={cn('text-2xl md:text-3xl')}>{icon}</span>
+              <span className={cn('text-lg md:text-xl')}>{text}</span>
             </div>)}
           </div>
 
           <h2 className={cn(
             'mt-8 mb-4 text-3xl text-transparent font-semibold bg-clip-text',
             'animate-background-shine bg-gradient-FVW',
-          )}>
-            Social Media
-          </h2>
+          )}>Social Media</h2>
 
           <div className={cn('flex flex-wrap gap-2')}>
             {Links.map(({ text, href, icon }) => <Link key={href} href={href} className={cn(
-              'px-2 py-3 flex justify-center items-center gap-3 rounded transition',
+              'px-2 py-2 md:py-3 flex justify-center items-center gap-3 rounded transition',
               'bg-action-button',
             )}>
-              <span className={cn('text-2xl')}>{icon}</span>
-              <span className={cn('text-xl text-wrap')}>{text}</span>
+              <span className={cn('text-xl md:text-2xl')}>{icon}</span>
+              <span className={cn('text-lg md:text-xl text-wrap')}>{text}</span>
             </Link>)}
           </div>
         </div>
