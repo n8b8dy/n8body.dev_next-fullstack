fd7e2a6209073145bef5e2867e7aa250e9d3de73
n8b8dy <ruslanchernomorski@gmail.com>
2024-05-20 03:00:14 +0300

    Fix: Return gradient to Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,9 @@ export default function Home() {
     <Fragment>
       <div className={cn(
         'relative w-full h-[calc(100vh-62px)] flex justify-center items-center',
-        'before:bg-gradient-FVW after:before:bg-gradient-FVW bg-pattern-circuit',
+        'before:absolute before:w-full before:h-1/6 before:left-0 before:top-0 before:bg-gradient-to-t before:from-transparent before:to-neutral-50 before:dark:to-neutral-950',
+        'after:absolute after:w-full after:h-1/3 after:left-0 after:bottom-0 after:bg-gradient-to-b after:from-transparent after:to-neutral-50 after:dark:to-neutral-950',
+        'bg-pattern-circuit',
       )}>
         <HeroSection/>
       </div>
@@ -46,3 +48,4 @@ export default function Home() {
     </Fragment>
   )
 }
+
