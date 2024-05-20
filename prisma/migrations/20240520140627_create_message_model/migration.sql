211bfd2633680be0586e82c57216c350bafe56a0
n8b8dy <ruslanchernomorski@gmail.com>
2024-05-20 17:12:45 +0300

    Feat: Create Message model

---
+++ prisma/migrations/20240520140627_create_message_model/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
