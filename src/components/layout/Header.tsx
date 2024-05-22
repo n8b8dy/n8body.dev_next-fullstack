ab395ed12c0b404255125288103d4ba461ac25e2
n8b8dy <ruslanchernomorski@gmail.com>
2024-05-22 17:03:43 +0300

    Fix: Use header tag in Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,13 +6,13 @@ import { cn } from '@/utils/styles'
 export const Header: FC = () => {
   return (
     <div className={cn('w-full min-h-[62px] flex justify-center bg-neutral-50 dark:bg-neutral-950 bg-opacity-25 border-b-[1px] dark:border-neutral-800')}>
-      <div className={cn('w-full max-w-screen-2xl px-5 py-2 flex justify-between items-center')}>
+      <header className={cn('w-full max-w-screen-2xl px-5 py-2 flex justify-between items-center')}>
         <Navbar/>
 
         <div>
           <ThemeSwitch/>
         </div>
-      </div>
+      </header>
     </div>
   )
 }
