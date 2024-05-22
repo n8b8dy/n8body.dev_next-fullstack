cf3dbc54bf294a578e1304dffe64f2fdc4f6b14b
n8b8dy <ruslanchernomorski@gmail.com>
2024-05-22 23:39:01 +0300

    Feat: Center vertically divs on Contacts page

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -23,8 +23,8 @@ const Links = [
 
 export default function Contacts() {
   return (
-    <Section className={cn('max-w-screen-2xl my-auto py-8 lg:flex-row lg:gap-[2%]')}>
-      <div className={cn('px-2 py-4 items-center lg:flex-[0_0_48%]')}>
+    <Section className={cn('max-w-screen-xl items-center my-auto py-8 lg:flex-row lg:gap-[2%]')}>
+      <div className={cn('px-2 py-4 items-center lg:flex-[0_0_49%]')}>
         <div className={cn('max-w-lg mx-auto')}>
           <h2 className={cn(
             'mb-4 text-3xl text-transparent font-semibold bg-clip-text',
@@ -34,14 +34,14 @@ export default function Contacts() {
         </div>
       </div>
 
-      <div className={cn('px-2 py-4 lg:flex-[0_0_48%]')}>
+      <div className={cn('px-2 py-4 lg:flex-[0_0_49%]')}>
         <div className={cn('max-w-lg mx-auto')}>
           <h2 className={cn(
             'mb-4 text-3xl text-transparent font-semibold bg-clip-text',
             'animate-background-shine bg-gradient-FVW',
           )}>Contacts</h2>
 
-          <div className={cn('pb-4 flex flex-col gap-4 overflow-x-auto')}>
+          <div className={cn('pt-1.5 pb-4 flex flex-col gap-4 overflow-x-auto')}>
             {Paragraphs.map(({ text, icon }) => <div key={text} className={cn('flex items-center gap-1.5')}>
               <span className={cn('text-2xl md:text-3xl')}>{icon}</span>
               <span className={cn('text-lg md:text-xl')}>{text}</span>
@@ -53,9 +53,9 @@ export default function Contacts() {
             'animate-background-shine bg-gradient-FVW',
           )}>Social Media</h2>
 
-          <div className={cn('flex flex-wrap gap-2')}>
+          <div className={cn('max-w-[296px] flex items-stretch flex-wrap gap-2')}>
             {Links.map(({ text, href, icon }) => <Link key={href} href={href} className={cn(
-              'px-2 py-2 md:py-3 flex justify-center items-center gap-3 rounded transition',
+              'min-w-32 md:min-w-36 py-2 md:py-3 flex justify-center items-center gap-3 rounded transition',
               'bg-action-button',
             )}>
               <span className={cn('text-xl md:text-2xl')}>{icon}</span>
