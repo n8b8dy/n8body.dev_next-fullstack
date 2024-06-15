e80192a15fe685afd23fa4e2bdc6f9637c27e0b6
n8b8dy <ruslanchernomorski@gmail.com>
2024-06-15 20:40:36 +0300

    Feat: Setup NextAuth

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,18 @@
 import type { PropsWithChildren } from 'react'
-import { ThemeProvider } from 'next-themes'
 
-export function Providers({ children }: PropsWithChildren) {
+import { getServerSession } from 'next-auth'
+
+import { ThemeProvider } from 'next-themes'
+import { SessionProvider } from '@/providers /SessionProvider'
+
+export async function Providers({ children }: PropsWithChildren) {
+  const session = await getServerSession()
+
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      {children}
+      <SessionProvider session={session}>
+        {children}
+      </SessionProvider>
     </ThemeProvider>
   )
 }
---
+++ environment.d.ts
@@ -0,0 +1,14 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    DATABASE_URL: string
+
+    NEXT_AUTH_URL: string
+    NEXTAUTH_SECRET: string
+    GITHUB_APP_CLIENT_ID: string
+    GITHUB_APP_CLIENT_SECRET: string
+
+    ADMIN_EMAIL: string
+
+    NEXT_PUBLIC_IP_ENDPOINT: string
+  }
+}
